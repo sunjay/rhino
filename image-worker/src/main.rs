cc369e71209cd130a2854a236b80a3496e0d89a8
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-17 11:03:10 -0500

    Started to try and get some basic rust code working. Waiting on https://github.com/rust-lang/rust/pull/37793 before I can continue

--- image-worker/src/main.rs
+++ image-worker/src/main.rs
@@ -1,3 +1,30 @@
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+extern crate rustc_serialize;
+
+extern crate image;
+
+use std::io::BufRead;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum Action {
+    Undo,
+    Redo,
+    Crop {x: u32, y: u32, width: u32, height: u32},
+    FlipHorizontal,
+    FlipVertical,
+    Resize {width: u32, height: u32},
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", serde_json::to_string(&Action::Undo).unwrap());
+    //let stdin = io::stdin();
+    //for line in stdin.lock().lines() {
+    //    let line = line.unwrap();
+    //}
 }
