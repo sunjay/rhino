3bf1d5bed66989ea44853d87ad93c078afdb7666
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-23 13:31:33 -0500

    Added additional required actions

--- image-worker/src/action.rs
+++ image-worker/src/action.rs
@@ -1,12 +1,24 @@
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Direction {
+    N, NE, E, SE, S, SW, W, NW,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Action {
     New {width: u32, height: u32},
+    // full path including extension
+    Load {path: String},
+    Save {path: String},
     Undo,
     Redo,
     Crop {x: u32, y: u32, width: u32, height: u32},
     FlipHorizontal,
     FlipVertical,
     Resize {width: u32, height: u32},
+    ResizeCanvas {width: u32, height: u32, anchor: Direction},
+    Rotate90Clockwise,
+    Rotate90Counterclockwise,
+    Rotate180,
 }
 
 #[cfg(test)]
@@ -22,6 +34,22 @@ mod tests {
         test_json(
             Action::New { width: 150, height: 250 },
             "{\"New\":{\"width\":150,\"height\":250}}"
+        );
+    }
+
+    #[test]
+    fn json_load() {
+        test_json(
+            Action::Load { path: "images/folder/foo.jpg".to_owned() },
+            "{\"Load\":{\"path\":\"images/folder/foo.jpg\"}}"
+        );
+    }
+
+    #[test]
+    fn json_save() {
+        test_json(
+            Action::Save { path: "images/folder/bar.png".to_owned() },
+            "{\"Save\":{\"path\":\"images/folder/bar.png\"}}"
         );
     }
 
@@ -70,6 +98,38 @@ mod tests {
         test_json(
             Action::Resize { width: 250, height: 550 },
             "{\"Resize\":{\"width\":250,\"height\":550}}"
+        );
+    }
+
+    #[test]
+    fn json_resize_canvas() {
+        test_json(
+            Action::ResizeCanvas { width: 250, height: 550, anchor: Direction::NE },
+            "{\"ResizeCanvas\":{\"width\":250,\"height\":550,\"anchor\":\"NE\"}}"
+        );
+    }
+
+    #[test]
+    fn json_rotate_90_clockwise() {
+        test_json(
+            Action::Rotate90Clockwise,
+            "\"Rotate90Clockwise\""
+        );
+    }
+
+    #[test]
+    fn json_rotate_90_counterclockwise() {
+        test_json(
+            Action::Rotate90Counterclockwise,
+            "\"Rotate90Counterclockwise\""
+        );
+    }
+
+    #[test]
+    fn json_rotate_180() {
+        test_json(
+            Action::Rotate180,
+            "\"Rotate180\""
         );
     }
 
