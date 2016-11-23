866e1657e2de11e010f2f967c93906249774f6c0
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-23 14:52:43 -0500

    Implemented undo/redo stack

--- image-worker/src/project.rs
+++ image-worker/src/project.rs
@@ -1,34 +1,75 @@
-use action::Action;
 use commands::Command;
+
+// Unrelated error types that should not be grouped
+// into one enum
+pub struct UndoStackEmpty;
+pub struct RedoStackEmpty;
+pub struct LoadFailed {
+    reason: &'static str,
+}
+pub struct SaveFailed {
+    reason: &'static str,
+}
+
+// The result of a mutation either succeeds or fails with
+// some error type
+pub type MutationResult<E> = Result<(), E>;
 
 pub struct Project<'a> {
     // undo stack has latest command at the end
     // and oldest command at the beginning
     undo_stack: Vec<&'a Command>,
-    // redo stack is emptied every time an action
+    // redo stack is emptied every time a command
     // is performed
     // otherwise it contains the most recently undone
     // command at the top (last item)
     redo_stack: Vec<&'a Command>,
 }
 
-impl Project {
-    fn perform_action(&mut self, action: Action) {
+impl<'a> Project<'a> {
+    pub fn new(width: u32, height: u32) -> Project<'a> {
+        Project {
+            undo_stack: Vec::new(),
+            redo_stack: Vec::new(),
+        }
+    }
+
+    pub fn perform_command(&mut self, command: Command) {
+        command.forward(self);
+        self.undo_stack.push(command);
         self.redo_stack.clear();
     }
 
-    fn undo(&mut self) {
-        //TODO: Deal with empty
-        let command = self.undo_stack.pop();
-        command.backward(&mut self);
+    pub fn undo(&mut self) -> MutationResult<UndoStackEmpty> {
+        if self.undo_stack.is_empty() {
+            return Err(UndoStackEmpty);
+        }
+
+        let command = self.undo_stack.pop().unwrap();
+        command.backward(self);
         self.redo_stack.push(command);
+
+        Ok(())
     }
 
-    fn redo(&mut self) {
-        //TODO: Deal with empty
-        let command = self.redo_stack.pop();
-        command.forward(&mut self);
+    pub fn redo(&mut self) -> MutationResult<RedoStackEmpty> {
+        if self.redo_stack.is_empty() {
+            return Err(RedoStackEmpty);
+        }
+
+        let command = self.redo_stack.pop().unwrap();
+        command.forward(self);
         self.undo_stack.push(command);
+
+        Ok(())
+    }
+
+    pub fn load(&mut self, path: String) -> MutationResult<LoadFailed> {
+        unimplemented!();
+    }
+
+    pub fn save(&mut self, path: String) -> MutationResult<SaveFailed> {
+        unimplemented!();
     }
 }
 
