a8ab82c544626d640a598ffce2ff2eaa041539e1
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-28 11:26:51 -0500

    Making image worker input processing more sophisticated

--- app/services/imageWorker.jsx
+++ app/services/imageWorker.jsx
@@ -8,6 +8,7 @@ const {
 class ImageWorker {
   constructor() {
     this.worker = null;
+    this.queue = [''];
   }
 
   start({dispatch}) {
@@ -16,16 +17,24 @@ class ImageWorker {
     });
 
     this.worker.stdout.on('data', (data) => {
-      const response = JSON.parse(data);
+      // The data that comes in is not guarenteed to be a complete command line
+      // To account for this, we maintain a queue of commands since we
+      // may get several complete commands or no complete commands in this data
+      // The final element in the queue always contains the latest incomplete
+      // response
+      // When a response is completed with the \n, this split call will result
+      // in an empty string at the end because 'abc\n'.split('\n') == ['abc', '']
+      // That empty string at the end will become the next latest incomplete
+      // response
+      // Even if we get something like 'abc\nfoo', foo is incomplete because
+      // it has not been terminated with a newline. Thus, it will remain at the
+      // end of the queue and keep being appended to until it is complete
+      const lines = data.toString().split('\n');
+      this.queue[this.queue.length - 1] += lines[0];
+      this.queue.push(...lines.slice(1));
 
-      if (response.Success) {
-        const {path, width, height, data} = response.Success;
-        dispatch(updateImage(path, width, height, data));
-      }
-      else {
-        console.error('image worker response', response);
-        //TODO: Implement something better here in #36
-        alert('Something went wrong. Check console for more information.');
+      for (const line of this.queue.slice(0, -1)) {
+        this.processResponse(dispatch, line);
       }
     });
 
@@ -44,6 +53,20 @@ class ImageWorker {
     this.worker.on('error', (error) => {
       console.error(`image worker error: ${error}`);
     });
+  }
+
+  processResponse(dispatch, data) {
+    const response = JSON.parse(data);
+
+    if (response.Success) {
+      const {path, width, height, data} = response.Success;
+      dispatch(updateImage(path, width, height, data));
+    }
+    else {
+      console.error('image worker response', response);
+      //TODO: Implement something better here in #36
+      alert('Something went wrong. Check console for more information.');
+    }
   }
 
   middleware() {
