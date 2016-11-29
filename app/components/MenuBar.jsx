bd1f2eb3eebe578a9252018dda25172262a75644
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-28 20:27:29 -0500

    Added buttons on the right of the menubar

--- app/components/MenuBar.jsx
+++ app/components/MenuBar.jsx
@@ -3,7 +3,11 @@ const React = require('react');
 const Navbar = require('./Navbar');
 const NavbarButton = require('./NavbarButton');
 
-const {menuBar} = require('../../scss/components/menuBar.scss');
+const {
+  menuBar,
+  menuBarRight,
+  menuBarLarge,
+} = require('../../scss/components/menuBar.scss');
 
 const MenuBar = () => (
   <Navbar className={menuBar}>
@@ -12,6 +16,18 @@ const MenuBar = () => (
     <NavbarButton>Image</NavbarButton>
     <NavbarButton>View</NavbarButton>
     <NavbarButton>Help</NavbarButton>
+
+    <div className={menuBarRight}>
+      <NavbarButton>
+        <span className={menuBarLarge}>&ndash;</span>
+      </NavbarButton>
+      <NavbarButton>
+        <span className={menuBarLarge}>+</span>
+      </NavbarButton>
+      <NavbarButton>
+        <span className={menuBarLarge}>&times;</span>
+      </NavbarButton>
+    </div>
   </Navbar>
 );
 
