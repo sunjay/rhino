d8d4c4eb60dd7e2372694aca834c5aaef0ae8755
Sunjay Varma <varma.sunjay@gmail.com>
2017-06-19 14:11:19 -0400

    Upgraded to webpack@3.0.0!

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
